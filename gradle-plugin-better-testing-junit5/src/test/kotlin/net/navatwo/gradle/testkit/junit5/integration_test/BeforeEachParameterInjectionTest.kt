2c59dfe4f0e2d286a26b8e501c1ede552bce7471
Kevin Brightwell <kevin.brightwell2@gmail.com>
2023-08-23 09:05:31 -0400

    fix import

--- gradle-plugin-better-testing-junit5/src/test/kotlin/net/navatwo/gradle/testkit/junit5/integration_test/BeforeEachParameterInjectionTest.kt
+++ gradle-plugin-better-testing-junit5/src/test/kotlin/net/navatwo/gradle/testkit/junit5/integration_test/BeforeEachParameterInjectionTest.kt
@@ -2,7 +2,7 @@ package net.navatwo.gradle.testkit.junit5.integration_test
 
 import net.navatwo.gradle.testkit.junit5.GradleProject
 import net.navatwo.gradle.testkit.junit5.GradleProject.Root
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.nio.file.Path
@@ -18,6 +18,6 @@ class BeforeEachParameterInjectionTest {
   @Test
   @GradleProject("default-project-root")
   fun `verify setup has parameters`(@Root rootPath: Path) {
-    Assertions.assertThat(fromSetup).isEqualTo(rootPath)
+    assertThat(fromSetup).isEqualTo(rootPath)
   }
 }

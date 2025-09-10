2949d496f5378ce870f88767d2734121a009d82a
Kevin Brightwell <kevin.brightwell@faire.com>
2025-09-10 16:22:46 -0400

    update publishing to use new plugin

--- build-logic/src/main/kotlin/better-testing.kotlin.gradle.kts
+++ build-logic/src/main/kotlin/better-testing.kotlin.gradle.kts
@@ -1,20 +1,28 @@
 import org.gradle.kotlin.dsl.invoke
 
 plugins {
-  `maven-publish`
-  signing
-
   kotlin("jvm")
 
   id("io.gitlab.arturbosch.detekt")
   id("com.jaredsburrows.license")
-
-  id("better-testing.versioning")
+  id("com.vanniktech.maven.publish")
 }
 
 repositories {
   mavenCentral()
 }
+
+group = "net.navatwo.gradle"
+version = "0.0.6-SNAPSHOT"
+
+val isRelease = providers.environmentVariable("RELEASE")
+  .map { it.isNotBlank() }
+  .getOrElse(false)
+
+if (isRelease) {
+  version = version.toString().substringBefore("-SNAPSHOT")
+}
+
 
 val javaVersion: Provider<String> = providers.fileContents(
   rootProject.layout.projectDirectory.file(".java-version"),
@@ -55,46 +63,40 @@ licenseReport {
   generateJsonReport = false
 }
 
-java {
-  withJavadocJar()
-  withSourcesJar()
-}
+// java artifacts (javadoc, sources) are now handled by the Vanniktech plugin
 
-publishing {
-  publications {
-    create<MavenPublication>("maven") {
-      from(components["java"])
+mavenPublishing {
+  publishToMavenCentral()
 
-      pom {
-        name.set("gradle-plugin-better-testing")
-        description.set("Test fixtures for testing Gradle Plugins")
-        url.set("https://github.com/Nava2/gradle-plugin-better-testing")
-
-        licenses {
-          license {
-            name.set("MIT License")
-            url.set("https://opensource.org/licenses/MIT")
-          }
-        }
-
-        developers {
-          developer {
-            id.set("Nava2")
-            name.set("Kevin Brightwell")
-            email.set("kevin.brightwell2+gradle-plugin-better-testing@gmail.com")
-          }
-        }
-
-        scm {
-          url.set("https://github.com/Nava2/gradle-plugin-better-testing.git")
-        }
-      }
-    }
+  if (isRelease) {
+    signAllPublications()
   }
 }
 
-signing {
-  sign(publishing.publications["maven"])
+mavenPublishing {
+  pom {
+    name.set("gradle-plugin-better-testing")
+    description.set("Test fixtures for testing Gradle Plugins")
+    url.set("https://github.com/Nava2/gradle-plugin-better-testing")
+    inceptionYear.set("2023")
+    licenses {
+      license {
+        name.set("MIT License")
+        url.set("https://opensource.org/licenses/MIT")
+      }
+    }
+    developers {
+      developer {
+        id.set("Nava2")
+        name.set("Kevin Brightwell")
+        email.set("kevin.brightwell2+gradle-plugin-better-testing@gmail.com")
+      }
+    }
+
+    scm {
+      url.set("https://github.com/Nava2/gradle-plugin-better-testing.git")
+    }
+  }
 }
 
 val testKitDirectory: Directory = rootProject.layout.projectDirectory.dir(".gradle/testKit")
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,9 @@
 plugins {
-  `maven-publish`
-  signing
-
   alias(libs.plugins.dependencyanalysis)
   alias(libs.plugins.kotlinx.binary.compatibility.validator)
 
-  alias(libs.plugins.nexus.publish)
-
-  id("better-testing.versioning")
+  alias(libs.plugins.maven.publish) apply false
+  alias(libs.plugins.kotlin) apply false
 }
 
 buildscript {
@@ -23,18 +19,6 @@ repositories {
 
 apiValidation {
   ignoredProjects += "gradle-plugin-better-testing"
-}
-
-nexusPublishing {
-  repositories {
-    sonatype {
-      nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-      snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
-      
-      username.set(findProperty("sonatypeUsername") as String? ?: System.getenv("SONATYPE_USERNAME"))
-      password.set(findProperty("sonatypePassword") as String? ?: System.getenv("SONATYPE_PASSWORD"))
-    }
-  }
 }
 
 extensions.findByName("buildScan")?.withGroovyBuilder {
